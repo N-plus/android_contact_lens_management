81d7085b9675cfe9476b06091490043123f815b6
Matsuzaki Yuto <matsuzaki@MatsuzakinoMacBook-Air.local>
2025-12-29 23:07:07 +0900

    20251230 v2

--- android/app/src/main/kotlin/com/example/contact_lens_management/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.contact_lens_management
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/jp/appinitylab/contactlens/MainActivity.kt
@@ -0,0 +1,3 @@
+package jp.appinitylab.contactlens
+import io.flutter.embedding.android.FlutterActivity
+class MainActivity : FlutterActivity()
